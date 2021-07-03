5391d1425f23b8b421e97ade1f11d2e9589b47e0
Srinath <srinathvangari@gmail.com>
2021-07-03 14:18:53 +0530

    month+1

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,95 @@ import * as adminfunctions from "firebase-admin";
 const rf = functions.region("asia-east2");
 
 adminfunctions.initializeApp(functions.config().firebase);
+export const creatRouteSummary = rf.https.onRequest((_request, response) => {
+    console.log("CALLED creatRouteSummary 1 : Date.now() "+Date.now());
+    let ii=0;
+    const dddate = new Date();
+    const dddate4 = new Date(Date.UTC(dddate.getUTCFullYear(),
+    dddate.getUTCMonth(),
+    dddate.getUTCDate()-1, 18, 30, 0, 0));
+    console.log("dddate1 : "+dddate4);
+    const epochy = Date.parse(dddate4.toString());
+    console.log("epochDate  : "+epochy);
+    let totalCow =0;
+    let totalBuf =0;
+    let totalCustomerOnVacation =0;
+    let totalCustomer =0;
+    let cowPrice =0;
+    let bufPrice =0;
+    let productObject: any;
+    adminfunctions.firestore().collection("routelist").
+    where("isActive", "==", "YES").
+    get().
+    then((querySnapshot) => {
+         querySnapshot.docs.map(async (routelistDoc) => {
+            const routeObject = JSON.parse(JSON.stringify(routelistDoc.data()));
+            const myJsonObject1= {
+                TCowLoaded: "TCowLoaded",
+             };
+            const myJsonObject = JSON.parse(JSON.stringify(myJsonObject1));
+            myJsonObject["TCowLoaded"] = totalCow;
+            await adminfunctions.firestore().collection("products").
+            where("isActive", "==", "YES").get().
+            then((querySnapshot) => {
+                 querySnapshot.docs.map( (doc5) => {
+                productObject = JSON.parse(JSON.stringify(doc5.data()));
+                  });
+                });
+           await adminfunctions.firestore().collection("dailydeliveries").
+            where("deliveryDate", "==", epochy).
+            where("route", "==", routeObject["routeName"]).
+            get().
+            then((querySnapshot) => {
+                 querySnapshot.docs.map(async (doc) => {
+                    ii=ii+1;
+                    totalCustomer =totalCustomer+1;
+            console.log("creatRouteSummary :ii : "+ii+": doc.id : "+doc.id);
+            const ddObject = JSON.parse(JSON.stringify(doc.data()));
+            const onVacation1 = ddObject["onVacation"];
+            if (onVacation1 == "YES") {
+                const cow1 = ddObject["cow"];
+                const buf1 = ddObject["buf"];
+                totalCow = totalCow + cow1;
+                totalBuf = totalBuf + buf1;
+            console.log("totalCow: "+totalCow+" totalBuf: "+totalBuf);
+            } else {
+            totalCustomerOnVacation= totalCustomerOnVacation+1;
+            }
+            });
+            });
+            cowPrice= productObject["cowPrice"];
+            bufPrice= productObject["bufPrice"];
+            myJsonObject["TCowLoaded"] = totalCow;
+            myJsonObject["TBufLoaded"] = totalBuf;
+            myJsonObject["TCustOnVacation"] = totalCustomerOnVacation;
+            myJsonObject["TDBufPrice"] = bufPrice;
+            myJsonObject["TDCowPrice"] = cowPrice;
+            myJsonObject["cowPrice"] = cowPrice;
+            myJsonObject["bufPrice"] = bufPrice;
+            myJsonObject["totalCustomers"] = totalCustomer;
+            myJsonObject["empName"] = routeObject["empName"];
+            myJsonObject["empMobile"] = routeObject["empMobile"];
+            myJsonObject["empId"] = routeObject["empEmail"];
+            myJsonObject["route"] = routeObject["routeName"];
+            myJsonObject["createdate"] = epochy;
+            myJsonObject["updateddate"] = epochy;
+            myJsonObject["createdby"] = "cronjob";
+            myJsonObject["updatedby"] = "cronjob";
+            myJsonObject["dStatus"] = "started";
+            myJsonObject["dStartTime"] = 0;
+            myJsonObject["dEndTime"] = 1;
+            myJsonObject["month"] = 7;
+            myJsonObject["empToken"] = "No Token";
+            myJsonObject["TBufDelivered"] = 0;
+            myJsonObject["TCowDelivered"] = 0;
+            myJsonObject["deliveryDate"] =epochy;
+            await adminfunctions.firestore()
+            .doc("ddroutesummary/"+ii+Date.now()).set(myJsonObject);
+                  });
+    });
+});
+
 export const createDailyDelivery = rf.https.onRequest((_request, response) => {
 console.log("CALLED ecreateDailyDelivery 21 : Date.now() "+Date.now());
 let ii=0;
@@ -14,26 +103,40 @@ dddate.getUTCDate()-1, 18, 30, 0, 0));
 console.log("dddate1 : "+dddate4);
 const epochy = Date.parse(dddate4.toString());
 console.log("epochDate  : "+epochy);
+let productObject: any;
+const totalCow =0;
 adminfunctions.firestore().collection("customers1").get().
 then((querySnapshot) => {
-    const tempDoc = querySnapshot.docs.map(async (doc) => {
+    querySnapshot.docs.map(async (doc) => {
         ii=ii+1;
      console.log("createDailyDelivery :ii : "+ii+": doc.id : "+doc.id);
      const myJsonObject1= {
         custId: "custId",
      };
+    await adminfunctions.firestore().collection("products").
+     where("isActive", "==", "YES").get().
+     then((querySnapshot) => {
+          querySnapshot.docs.map( (doc) => {
+             ii=ii+1;
+          console.log("products :ii : "+ii+": doc.id : "+doc.id);
+         console.log("productsJSON.stringify(doc.data):  "+
+         JSON.stringify(doc.data()));
+         productObject = JSON.parse(JSON.stringify(doc.data()));
+         console.log("productObject :  "+productObject);
+     console.log("JSON.stringify(doc.data()):  "+JSON.stringify(doc.data()));
+           });
+         });
     console.log("JSON.stringify(doc.data)  :  "+JSON.stringify(doc.data()));
     const userObject = JSON.parse(JSON.stringify(doc.data()));
    const vstart = userObject["vStartDate"];
    const vend = userObject["vEndDate"];
    const myJsonObject = JSON.parse(JSON.stringify(myJsonObject1));
-   myJsonObject["empName"] = "";
-   myJsonObject["empMobile"] = "";
-   myJsonObject["cow"] = 0;
-   myJsonObject["buf"] = 0;
-   myJsonObject["cowPrice"] = 0;
-   myJsonObject["bufPrice"] = 0;
-   myJsonObject["empId"] = "";
+   myJsonObject["cow"] = userObject["cow"];
+   const cow1 = userObject["cow"];
+   console.log("cow1: "+cow1);
+   myJsonObject["buf"] = userObject["buf"];
+   myJsonObject["cowPrice"] = productObject["cowPrice"];
+   myJsonObject["bufPrice"] = productObject["bufPrice"];
     myJsonObject["deliveryDate"] = epochy;
     myJsonObject["createdate"] = epochy;
     myJsonObject["updateddate"] = epochy;
@@ -43,22 +146,41 @@ then((querySnapshot) => {
     myJsonObject["custId"] = userObject["uuid"];
     myJsonObject["custName"] = userObject["custName"];
     myJsonObject["custMobile"] = userObject["custMobile"];
-    myJsonObject["custAddress"] = "Palladium Homes";
+    myJsonObject["custAddress"] = userObject["custAddress"];
     myJsonObject["custToken"] = userObject["custToken"];
-    myJsonObject["route"] ="";
+    myJsonObject["route"] =userObject["route"];
+    const route1 = userObject["route"];
+    await adminfunctions.firestore().collection("routelist").
+    where("routeName", "==", route1).
+    where("isActive", "==", "YES").get().
+    then((querySnapshot) => {
+         querySnapshot.docs.map( (doc) => {
+            ii=ii+1;
+         console.log("routelist :ii : "+ii+": doc.id : "+doc.id);
+        console.log("routelist.stringify(doc.data):  "+
+        JSON.stringify(doc.data()));
+        const routelistObject = JSON.parse(JSON.stringify(doc.data()));
+        console.log("routelistObject :  "+routelistObject);
+        myJsonObject["empName"] = routelistObject["empName"];
+   myJsonObject["empMobile"] = routelistObject["empMobile"];
+   myJsonObject["empId"] = routelistObject["empEmail"];
+          });
+        });
     myJsonObject["month"] = month+1;
     myJsonObject["onVacation"]="no";
    console.log("(vstart < Date.now() && Date.now() < vend) : "+
    (vstart <= Date.now() && Date.now() <=vend));
    if (vstart < Date.now() && Date.now() < vend) {
     console.log(myJsonObject["custName"] +" is on Vacation today");
-    myJsonObject["onVacation"]="yes";
+    myJsonObject["onVacation"]="YES";
+   } else {
+    console.log(myJsonObject["custName"] +" is NOT NOT on Vacation today");
    }
      await adminfunctions.firestore()
     .doc("dailydeliveries/"+ii+Date.now()).set(myJsonObject);
      return myJsonObject;
    });
-console.log("JSON.stringify(tempDoc) :  "+JSON.stringify(tempDoc));
+console.log("totalCow:  "+totalCow);
       });
 });
 
